385b7344799f1f6b619097943cf76a5861f590a3
etongfu <tongfu.e@xtalpi.com>
2021-10-19 10:04:45 +0800

    feat: add string

--- src/python/src/list.py
+++ src/python/src/list.py
@@ -4,24 +4,24 @@
 """
 
 
-list = [1,3, 5,3,2,12,123,123,123,123,12]
-print(list)
+list1 = [1,3, 5,3,2,12,123,123,123,123,12]
+print(list1)
 list2 = ['hello'] * 3
 print(list2)
 # 通过下标循环
-for index in range(len(list)):
-  print(index, list[index])
+for index in range(len(list1)):
+  print(index, list1[index])
     
-for item in list:
+for item in list1:
     print(item)
 
-for index, el in enumerate(list):
+for index, el in enumerate(list1):
   print(index, el)
   
 # 增删改查处理
-list.append(1000)
-list.insert(0, 999)
-print(list)
+list1.append(1000)
+list1.insert(0, 999)
+print(list1)
 
 list3 = [3]
 # list3.extend([11111, 2222])
@@ -33,8 +33,12 @@ if 0 in list3:
 list3.pop() # 不提供参数会默认删除最后一个
 print(list3)
 
-list.sort()
-print(list)
+list1.sort()
+print(list1)
 # sorted函数返回列表排序后的拷贝不会修改传入的列表， 函数的设计就应该像sorted函数一样尽可能不产生副作用
 list4 = sorted(list3)
 print(list4)
+# 统计计算
+print(max(list4))
+
+print(list(range(10)))
---
+++ src/python/src/string.py
@@ -0,0 +1,18 @@
+
+str1 = 'hello, python!'
+
+print(len(str1))
+
+# 获得字符串首字母大写的拷贝
+print(str1.capitalize()) 
+
+# 获得字符串每个单词首字母大写的拷贝
+print(str1.title())
+
+# 获得字符串变大写后的拷贝
+print(str1.upper())
+# 获得字符串变小写后的拷贝
+print(str1.lower())
+
+# 从字符串中查找子串所在位置
+print(str1.find('python'))
