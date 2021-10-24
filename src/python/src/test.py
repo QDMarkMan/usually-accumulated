56ff4282f1bdf33a3cb1f547dd17c5e8834e6cb4
etongfu <531202571@qq.com>
2021-10-24 16:13:46 +0800

    feat: add test.py case

--- src/python/src/test.py
+++ src/python/src/test.py
@@ -3,6 +3,7 @@
 """
 import os
 import time
+import random
 # 1: 在屏幕上显示跑马灯文字。
 def marquee_show():
     content = "This is python"
@@ -18,10 +19,20 @@ def marquee_show():
 
 # 2: 设计一个函数产生指定长度的验证码，验证码由大小写字母和数字构成。
 def random_sms_code(length = 4):
- print('radom_sms_code', length)
+  result = ''
+  base = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  for _ in range(length):
+    result +=  random.choice(base)
+  print('radom_sms_code', result, length)
+  return result
 
-random_sms_code()
+random_sms_code(20)
+
 # 3: 设计一个函数返回给定文件名的后缀名。
+def get_file_suffix(filename):
+  print('get_file_suffix', filename)
+
+get_file_suffix('abc.sdf')
 
 # 4: 设计一个函数返回传入的列表中最大和第二大的元素的值。
 
