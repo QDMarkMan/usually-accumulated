78d9c55d0b7c3315fa26f791365a26d05bf7f578
etongfu <tongfu.e@xtalpi.com>
2021-11-10 09:58:41 +0800

    feat: update adwancement

--- src/python/src/adwancement.py
+++ src/python/src/adwancement.py
@@ -5,6 +5,9 @@
 *  @Description 进阶特性, 比较重要的知识点.
 **************************************************************************
 """
+import heapq
+import itertools
+
 
 # 1：通过生成式生成列表，集合和字典
 
@@ -23,11 +26,67 @@ prices2 = { key: value for key, value in prices.items() if value > 100 }
 print('prices2', prices2)
 
 # 2：嵌套列表的坑
-names = ['关羽', '张飞', '赵云', '马超', '黄忠']
-courses = ['语文', '数学', '英语']
-scores = [[None] * len(courses) for _ in range(len(names))]
-for row, name in enumerate(names):
-  for col, course in enumerate(courses):
-     scores[row][col] = float(input(f'请输入{name}的{course}成绩: '))
-     print(scores)
+# names = ['关羽', '张飞', '赵云', '马超', '黄忠']
+# courses = ['语文', '数学', '英语']
+# scores = [[None] * len(courses) for _ in range(len(names))]
+# for row, name in enumerate(names):
+#   for col, course in enumerate(courses):
+#      scores[row][col] = float(input(f'请输入{name}的{course}成绩: '))
+#      print(scores)
  
+# 3：heapq 模块（推排序）
+"""
+从列表中找出最大的或最小的N个元素
+堆结构(大根堆/小根堆)
+"""
+list1 = [34, 25, 12, 99, 87, 63, 58, 78, 88, 92]
+list2 = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+print(heapq.nlargest(3, list1))
+print(heapq.nsmallest(3, list1))
+print(heapq.nlargest(2, list2, key=lambda x : x['price']))
+
+
+# 4：迭代工具模块
+# 产生ABCD的全排列
+itertools.permutations('ABCD')
+# 产生ABCDE的五选三组合
+itertools.combinations('ABCDE', 3)
+# 产生ABCD和123的笛卡尔积
+itertools.product('ABCD', '123')
+# 产生ABC的无限循环序列
+itertools.cycle(('A', 'B', 'C'))
+
+
+#5：collections模块
+"""
+namedtuple：命令元组，它是一个类工厂，接受类型的名称和属性列表来创建一个类。
+deque：双端队列，是列表的替代实现。Python中的列表底层是基于数组来实现的，而deque底层是双向链表，因此当你需要在头尾添加和删除元素是，deque会表现出更好的性能，渐近时间复杂度为$O(1)$。
+Counter：dict的子类，键是元素，值是元素的计数，它的most_common()方法可以帮助我们获取出现频率最高的元素。Counter和dict的继承关系我认为是值得商榷的，按照CARP原则，Counter跟dict的关系应该设计为关联关系更为合理。
+OrderedDict：dict的子类，它记录了键值对插入的顺序，看起来既有字典的行为，也有链表的行为。
+defaultdict：类似于字典类型，但是可以通过默认的工厂函数来获得键对应的默认值，相比字典中的setdefault()方法，这种做法更加高效。
+"""
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around',
+    'the', 'eyes', "don't", 'look', 'around', 'the', 'eyes',
+    'look', 'into', 'my', 'eyes', "you're", 'under'
+]
+
+from collections import Counter
+
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around',
+    'the', 'eyes', "don't", 'look', 'around', 'the', 'eyes',
+    'look', 'into', 'my', 'eyes', "you're", 'under'
+]
+counter = Counter(words)
+print(counter.most_common(3))
+
